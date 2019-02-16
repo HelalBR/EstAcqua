2a89e628fdf6a45eec7484d347f0dd47de971427
Alan Helal <alan@helal.com.br>
2019-02-16 13:37:40 -0200

    Criação do main.py

---
+++ NanoGateway/main.py
@@ -0,0 +1,7 @@
+/*
+	Arquivo principal do NanoGateway. É o arquivo carregado após o processo de boot do LoPy4 ter sido concluído
+	
+	Bibliotecas importadas:
+		config (Arquivo criado com as configurações a serem utilizadas - config.py)
+		nanogateway
+*/
