0c747ac56c0caadff380298d28e619120847ac00
Alan Helal <alan@helal.com.br>
2019-02-16 13:54:36 -0200

    Update main.py

--- NanoGateway/main.py
+++ NanoGateway/main.py
@@ -5,3 +5,28 @@
 		config (Arquivo criado com as configurações a serem utilizadas - config.py)
 		nanogateway
 */
+
+# Importando bibliotecas
+import config
+from nanogateway import NanoGateway
+
+# Criando uma instância do NanoGateway e configurando com os parâmetros definidos no config.py
+if __name__ == '__main__':
+    nanogw = NanoGateway(
+        id=config.GATEWAY_ID, # ID do Gateway que será autenticado na TTN
+        frequency=config.LORA_FREQUENCY, # Frequência a ser utilizada na transmissão LoRa
+        datarate=config.LORA_GW_DR, # Datarate utilizado
+        ssid=config.WIFI_SSID, # SSID da rede Wi-Fi que o LoPy4 se conectará para transmistir para a internet
+        password=config.WIFI_PASS, # Senha da rede Wi-Fi
+        server=config.SERVER, # Servidor utilizado da TTN
+        port=config.PORT, # Porta de acesso ao servidor da TTN
+        ntp_server=config.NTP, # Servidor NTP utilizado
+        ntp_period=config.NTP_PERIOD_S # Período, em segundos, entre as sincronizações com o NTP
+        )
+	
+	# inicialização da instância do NanoGateway
+    	nanogw.start()
+	
+	# Se estiver conectado via terminal ao LoPy4, pressione Enter para utilizar o REPL
+    	nanogw._log('Pressione Enter para utilizar o RPL')
+    	input()
