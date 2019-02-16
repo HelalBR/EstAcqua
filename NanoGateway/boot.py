05513412d5cfa779a89b051253c2d3208965cfc5
Alan Helal <alan@helal.com.br>
2019-02-16 13:10:23 -0200

    Criação do Boot.py

---
+++ NanoGateway/boot.py
@@ -0,0 +1,27 @@
+/*
+Arquivo de Boot do LoPy4 (utilizado como NanoGateway no projeto)
+
+Bibliotecas importadas:
+	machine
+	os
+	pycom
+*/
+
+# Importando as bibliotecas
+
+from machine import UART
+import machine
+import os
+import pycom
+
+# Código executado no boot do LoPy4
+
+# Desabilita o pisca-pisca do LED embutido na placa
+pycom.heartbeat(False)
+
+# Configura a UART
+uart = UART(0, baudrate=115200)
+os.dupterm(uart)
+
+# Qual arquivo .py executar após o processo de boot do LoPy4
+machine.main('main.py')
